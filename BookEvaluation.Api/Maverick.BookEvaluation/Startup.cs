9f4181d552200ccf9255c49248ff8f1170153e0b
xjMavericKjx <maximaxim@tpu.ru>
2022-03-11 13:46:19 +0300

    c

--- BookEvaluation.Api/Maverick.BookEvaluation/Startup.cs
+++ BookEvaluation.Api/Maverick.BookEvaluation/Startup.cs
@@ -8,7 +8,6 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
 
 namespace Maverick.BookEvaluation
 {
